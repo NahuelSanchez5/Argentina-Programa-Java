4e0f65599b6b55fbc460765b1feed89b87e2a7bf
NahuelSanchez5 <nahuel-sanchez@outlook.com>
2023-03-08 12:15:18 -0300

    Update Clase 2.java

--- Clase 2.java
+++ Clase 2.java
@@ -1,3 +1,17 @@
-public class Clase 2 {
-    
+char letras[] = {'a','b','c','d'};
+
+int numeros[] = new int[]{1,37,16};
+
+for(int i=0;i<numeros.length;i++){
+
+if (numeros[i] < 30) {
+
+System.out.println(letras[i+1]);
+
+} else {
+
+System.out.println(numeros[i]);
+
+}
+
 }
